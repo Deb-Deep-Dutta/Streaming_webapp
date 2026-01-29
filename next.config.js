2f926616c9eb2338e29956be34d2cce5ebdda728
Deb Deep Dutta <debdeepdutta42003@gmail.com>
2026-01-30 04:35:48 +0530

    CSP hardening

--- next.config.js
+++ next.config.js
@@ -2,19 +2,35 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  // Security headers
   async headers() {
     return [
       {
         source: '/(.*)',
         headers: [
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self'",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' https://image.tmdb.org data:",
+              "font-src 'self'",
+              "frame-src https://vidfast.pro https://vidfast.in https://vidfast.io https://vidfast.me https://vidfast.net https://vidfast.pm https://vidfast.xyz",
+              "connect-src 'self' https://api.themoviedb.org",
+              "media-src 'self'",
+              "object-src 'none'",
+              "base-uri 'self'",
+              "form-action 'self'",
+              "frame-ancestors 'self'"
+            ].join('; ')
           },
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
           },
           {
             key: 'Referrer-Policy',
